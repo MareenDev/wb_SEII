98a82ad7435ccb7a87e6774c7790da1567b72671
MareenDev <moinmoin1101@aol.com>
2022-12-13 13:41:26 +0100

    Erstellung Datei predict2.py, Änderung .gitignore

---
+++ src/predict2.py
@@ -0,0 +1,27 @@
+import os
+import pickle
+import numpy as np
+
+# Lesen der serialisierten Model-Daten
+
+path = os.path.join("data", "model", "model.sav")
+file_model = open(path, 'rb')
+loaded_model = pickle.load(file_model)
+file_model.close()
+
+# Lesen der Test-Daten
+
+# Ziel-Daten für Prädiktion nicht notwendig
+# path_test_target = os.path.join("data", "model", "testdata_target.npy")
+# data_test_target = np.loa read_csv(path_test_target)
+
+# Lesen der Test-Feature-Daten
+path_test_features = os.path.join("data", "model", "testdata_features.npy")
+data_test_features = np.load(path_test_features)
+
+# Prediktion auf Basis der Testdaten
+prediction = loaded_model.predict(data_test_features)
+print(prediction)
+
+path_test_prediction = os.path.join("data", "model", "testdata_prediction.npy")
+np.save(path_test_prediction, prediction)
